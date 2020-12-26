275fc42e6b20a3e2296f9ff8867b776a13076104
CodingOnBush <omranemostafa.pro@gmail.com>
2020-12-26 15:42:52 +0100

    Trying to use MVVM
    J'ai utilisé cette vidéo pour essayer : https://www.youtube.com/watch?v=1IlUBHvgY8Q

---
+++ Projet4A/TestView.swift
@@ -0,0 +1,53 @@
+//
+//  TestView.swift
+//  Projet4A
+//
+//  Created by Sogekingu on 26/12/2020.
+//
+
+import SwiftUI
+
+let apiURL = "https://www.googleapis.com/books/v1/volumes?q=search+terms"
+
+class BooksViewModel: ObservableObject {
+    
+    @Published var message = "Message from BooksViewModel"
+    
+    @Published var books: [Book] = [
+        Book(title: "Title", author: "Author")
+    ]
+    
+    func changement(){
+        self.message = "NEW"
+        self.books.append(Book(title: "New title", author: "New author"))
+    }
+    
+}
+
+struct TestView: View {
+    @ObservedObject var booksVM = BooksViewModel()
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                Text(booksVM.message)
+                ForEach(booksVM.books) { book in
+                    BookCellView(book: book)
+                }
+            }
+            .navigationTitle("Scroll view")
+            .navigationBarItems(trailing: Button(action: {
+                print("Fetching DATA")
+                self.booksVM.changement()
+            }, label: {
+                Text("Refresh")
+            }))
+        }
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
